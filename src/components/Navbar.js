9fc4a5902741498a0f467ce876b3f565ac89d590
Malek <johnmalek54@gmail.com>
2025-03-04 23:26:23 +0300

    Finish up the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,6 +13,15 @@ function Navbar(){
                 <li><a href="pages/about.js">About</a></li>
                 <li><a href="pages/signup.js">Sign Up</a></li>
             </ul>
+            
+            <div className="search-bar">
+                <input type="text" placeholder="What are you looking for?" />
+                <button className="search">🔍</button>
+            </div>
+            <div className="icons">
+                <span className="icon favorite">&#9825;</span>
+                <span className="icon cart">&#128722;</span>
+            </div>
         </nav>
         <hr className="hr"/>
        </>
